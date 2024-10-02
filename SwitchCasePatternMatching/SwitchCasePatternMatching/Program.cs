2aadc9049894b4435e2c7789f1b2106b81f39197
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-02 09:33:36 +0400

    pattern matching

---
+++ SwitchCasePatternMatching/SwitchCasePatternMatching/Program.cs
@@ -0,0 +1,13 @@
+﻿double gpa = 88.8;
+
+string grade = gpa switch
+{
+    >= 91 => "A",
+    >= 81 => "B",
+    >= 71 => "C",
+    >= 61 => "D",
+    >= 51 => "E",
+    _ => "F",
+};
+
+Console.WriteLine($"Your grade is {grade}");
