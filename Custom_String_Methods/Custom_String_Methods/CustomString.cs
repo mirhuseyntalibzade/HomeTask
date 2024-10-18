05794a17d85fb8c41e6d191501279f492d4708ee
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-18 10:00:14 +0400

    custom methods

---
+++ Custom_String_Methods/Custom_String_Methods/CustomString.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_String_Methods
+{
+    internal class CustomString
+    {
+        private string _input;
+        public CustomString(string input)
+        {
+            _input = input;
+        }
+        public string[] CustomSplit(char seperator)
+        {
+            string[] result = [];
+            string temp = "";
+            int index = 0;
+            for (int i = 0; i < _input.Length; i++)
+            {
+                if (seperator == _input[i])
+                {
+                    if (index >= result.Length)
+                    {
+                        Array.Resize(ref result, result.Length + 1);
+                    }
+                    result[index] = temp;
+                    index++;
+                    temp = "";
+                }
+                else
+                {
+                    temp += _input[i];
+                }
+
+            }
+            if (temp.Length > 0)
+            {
+                if (index >= result.Length)
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                }
+                result[index] = temp;
+            }
+
+            return result;
+        }
+        public string CustomReplace(char oldChar, char newChar)
+        {
+            string result = "";
+            for (int i = 0; i < _input.Length; i++)
+            {
+                if (_input[i] == oldChar)
+                {
+                    result += newChar;
+                }
+                else
+                {
+                    result += _input[i];
+                }
+            }
+            return result;
+        }
+
+        public int CustomLastIndexOf(string value)
+        {
+            int valueLength = value.Length;
+            int inputLength = _input.Length;
+
+            for (int i = inputLength - valueLength; i >= 0; i--)
+            {
+                bool matchFound = true;
+
+                for (int j = 0; j < valueLength; j++)
+                {
+                    if (_input[i + j] != value[j])
+                    {
+                        matchFound = false;
+                        break;
+                    }
+                }
+
+                if (matchFound)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ Custom_String_Methods/Custom_String_Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Custom_String_Methods
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            CustomString myString = new CustomString("The cat is on top of the mat");
+
+            string[] splitted = myString.CustomSplit('t');
+
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                Console.WriteLine(splitted[i]);
+            }
+
+            Console.WriteLine(myString.CustomReplace('m','p'));
+
+            Console.WriteLine(myString.CustomLastIndexOf("a"));
+        }
+    }
+}
