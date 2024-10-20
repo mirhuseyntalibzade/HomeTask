33530a8d32240775bd7671df46d4f613104e1a91
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-20 12:41:10 +0400

    weapon game

---
+++ WeaponGame/WeaponGame/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace WeaponGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon rifle = new Weapon();
+            Console.WriteLine(
+                """
+                0 - To get information
+                1 - For Shoot method
+                2 - For Fire method
+                3 - For GetRemainBulletCount method
+                4 - For Reload method
+                5 - For ChangeFireMode method
+                6 - For Exitting The Programme
+                """
+                );
+
+            string answer = default;
+
+            while (answer!="6")
+            {
+                answer = Console.ReadLine();
+
+                switch (answer)
+                {
+                    case "0":
+                        Console.WriteLine(
+                            $"""
+                        Maximum ammo capacity: {rifle.MaxAmmoCapacity}
+                        Current ammo: {rifle.CurrentAmmoCapacity}
+                        Rifle mode: {rifle.Mode}
+                        """
+                            );
+                        break;
+                    case "1":
+                        Console.WriteLine("Trying to shoot.");
+                        rifle.Shoot();
+                        break;
+                    case "2":
+                        Console.WriteLine("Trying to fire.");
+                        rifle.Fire();
+                        break;
+                    case "3":
+                        Console.WriteLine($"Current ammo: {rifle.CurrentAmmoCapacity}");
+                        break;
+                    case "4":
+                        Console.WriteLine("Reloading...");
+                        rifle.Reload();
+                        break;
+                    case "5":
+                        rifle.ChangeFireMode();
+                        break;
+                    case "6":
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input. Please try again");
+                        break;
+                }
+            }
+            Console.WriteLine("The programme has ended. Goodbye.");
+        }
+    }
+}
---
+++ WeaponGame/WeaponGame/Weapon.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponGame
+{
+    internal class Weapon
+    {
+        private int _maxAmmoCapacity = 40;
+
+        public Weapon()
+        {
+            _currentAmmoCapacity = _maxAmmoCapacity;
+        }
+        public int MaxAmmoCapacity
+        {
+            get
+            {
+                return _maxAmmoCapacity;
+            }
+            set
+            {
+                _maxAmmoCapacity = value;
+            }
+        }
+
+        private int _currentAmmoCapacity;
+        public int CurrentAmmoCapacity
+        {
+            get
+            {
+                return _currentAmmoCapacity;
+            }
+            set
+            {
+                _currentAmmoCapacity = value;
+            }
+        }
+
+        public string Mode { get; set; } = "single";
+        public void Shoot()
+        {
+            if (_currentAmmoCapacity > 0)
+            {
+                _currentAmmoCapacity--;
+                Console.WriteLine("The rifle has shot 1 round.");
+            }
+            else
+            {
+                Console.WriteLine("Out of ammo! Reload required.");
+            }
+        }
+        public void Fire()
+        {
+            if (Mode == "auto")
+            {
+                if (_currentAmmoCapacity > 0)
+                {
+                    Console.WriteLine($"The rifle has shot {_currentAmmoCapacity} rounds.");
+                    _currentAmmoCapacity = 0;
+                }
+                else
+                {
+                    Console.WriteLine("You have already emptied your magazine. Reload required!");
+                }
+            } else
+            {
+                Console.WriteLine("Your mode is on single.");
+                Shoot();
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            return _currentAmmoCapacity;
+        }
+        public void Reload()
+        {
+            if (_currentAmmoCapacity<=0)
+            {
+                _currentAmmoCapacity = _maxAmmoCapacity;
+                Console.WriteLine("Magazine reloaded.");
+            } else
+            {
+                Console.WriteLine("You still have left bullets in magazine.");
+            }
+        }
+        public void ChangeFireMode()
+        {
+            if (Mode == "single")
+            {
+                Mode = "auto";
+                Console.WriteLine("Change mode to auto.");
+            }
+            else
+            {
+                Mode = "single";
+                Console.WriteLine("Changed mode to single");
+            }
+        }
+    }
+}
