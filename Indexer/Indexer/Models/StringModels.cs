fb9945a4e564f5a6c96ff4899b8c7bd12a90fb2f
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-25 09:22:35 +0400

    indexer

---
+++ Indexer/Indexer/Models/StringModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer.Models
+{
+    internal class StringModels
+    {
+        string[] cities;
+        public StringModels()
+        {
+            cities = new string[0];
+        }
+
+        public void AddCity(string cityName) 
+        {
+            Array.Resize(ref cities, cities.Length + 1);
+            cities[cities.Length - 1] = cityName;
+        }
+
+        public int this[string cityName]
+        {
+            get {
+                return Array.IndexOf(cities, cityName);
+            }
+        }
+    }
+}
---
+++ Indexer/Indexer/Program.cs
@@ -0,0 +1,15 @@
+﻿using Indexer.Models;
+
+namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringModels cities = new StringModels();
+
+            cities.AddCity("Berlin");
+            Console.WriteLine(cities["Berlin"]);
+        }
+    }
+}
