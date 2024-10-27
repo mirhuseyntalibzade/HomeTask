e7a72692a54f49dcd87a443ebfa5fb76b2ab1aae
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-10-27 04:40:29 +0400

    reflection

---
+++ Reflection/Reflection/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Models
+{
+    internal class User
+    {
+        private int _id { get; set; }
+        private string _name { get; set; }
+        private string _surname { get; set; }
+        private static int _age { get; set; }
+
+        private void GetFullName()
+        {
+            Console.WriteLine($"{_name} {_surname}");
+        }
+
+        private static void ChangeAge(int age)
+        {
+            _age = age;
+        }
+    }
+}
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,33 @@
+﻿using Reflection.Models;
+using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            Type userType = typeof(User);
+            
+            PropertyInfo idProperty = userType.GetProperty("_id", BindingFlags.NonPublic | BindingFlags.Instance);
+            PropertyInfo nameProperty = userType.GetProperty("_name", BindingFlags.NonPublic | BindingFlags.Instance);
+            PropertyInfo surNameProperty = userType.GetProperty("_surname", BindingFlags.NonPublic | BindingFlags.Instance);
+            PropertyInfo ageProperty = userType.GetProperty("_age", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            MethodInfo getFullNameMethod = userType.GetMethod("GetFullName", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo changeAgeMethod = userType.GetMethod("ChangeAge", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            
+            idProperty.SetValue(user1, 0);
+            nameProperty.SetValue(user1, "Batman");
+            surNameProperty.SetValue(user1, "Batmanoğlu");
+            ageProperty.SetValue(user1, 15);
+            getFullNameMethod.Invoke(user1, null);
+            changeAgeMethod.Invoke(user1, new object[] { 45 });
+
+            Console.WriteLine(idProperty.GetValue(user1));
+            Console.WriteLine(nameProperty.GetValue(user1));
+            Console.WriteLine(surNameProperty.GetValue(user1));
+            Console.WriteLine(ageProperty.GetValue(user1));
+        }
+    }
+}
