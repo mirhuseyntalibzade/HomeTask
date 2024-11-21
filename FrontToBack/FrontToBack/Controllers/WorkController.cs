0cb1eada261091c1ef3955704459380c89c23b62
mirhuseyntalibzade <mirhuseynet-ab205@code.edu.az>
2024-11-21 04:10:55 +0400

    mvc front to back

---
+++ FrontToBack/FrontToBack/Controllers/WorkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBack.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
